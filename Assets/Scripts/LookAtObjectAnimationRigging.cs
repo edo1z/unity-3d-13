3d4378a784ad4b60d6d0efb8bc49d20e3ed71d79
web3ten0 <web3ten0@gmail.com>
2022-10-20 15:08:01 +0900

    Animation Riggingの有効無効の切り替え

---
+++ Assets/Scripts/LookAtObjectAnimationRigging.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class LookAtObjectAnimationRigging : MonoBehaviour
+{
+    private Rig rig;
+    private float targetWeight;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rig>();
+    }
+
+    private void Update()
+    {
+        rig.weight = Mathf.Lerp(rig.weight, targetWeight, Time.deltaTime * 10f);
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            targetWeight = 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            targetWeight = 0f;
+        }
+    }
+}
